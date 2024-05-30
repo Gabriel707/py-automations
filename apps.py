317c4a937246830f40c088c591e0e10e0e6deb16
Gabriel Araujo <gabrielsant95@gmail.com>
2024-05-29 22:47:55 -0300

    DEBUG: Testing Debug over IDE

--- apps.py
+++ apps.py
@@ -1,3 +1,10 @@
-from datetime import datetime
+print('Hello')
 
-print(datetime.now())
+
+def calcular_cardapio(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+
+calcular_cardapio(32.90, 13.90)
+print('Conta encerrada')
