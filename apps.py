f3636027972325ecbf4c1f74ca0e611fdf6e4b66
Gabriel Araujo <gabrielsant95@gmail.com>
2024-06-11 20:18:10 -0300

    CODE: Creating database connection functions

--- apps.py
+++ apps.py
@@ -1,4 +1,12 @@
 from models.usuario import Usuario
+from carro import ligar_carro
+from moto import ligar_moto
+from banco_de_dados import buscar_usuarios
+
+
+ligar_moto()
+ligar_carro()
+buscar_usuarios()
 
 cliente_1 = Usuario('Gabriel', 'santana', 29)
 usuario1 = Usuario("joao", "silva", 30)
@@ -7,6 +15,7 @@ usuario2 = Usuario("maria", "oliveira", 25)
 print(cliente_1)
 print(usuario1)
 print(usuario2)
+
 
 '''
 5. Guarde informações sobre a data de aniversário do funcionário(dd/mm/aaaa)
