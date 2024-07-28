191bb50541aa90db8a32259fc26b08b126218f9e
Gabriel707 <gabrielsant95@gmail.com>
2024-07-28 16:39:15 -0300

    Reviewing concepts

---
+++ loops_challenges.py
@@ -0,0 +1,18 @@
+# count = 1
+# while count <= 120:
+#     print(count)
+#     count += 1
+# print('Fim de loop')
+
+# senha = 'secreto'
+# sua_senha = input('Insira sua senha: ')
+# while sua_senha != senha:
+#     sua_senha = input('Insira sua senha: ')
+# print(f'Senha inserida {sua_senha} - Senha correta: {senha} - ACESSO CONCEDIDO')
+
+count = 101
+break_point = 1
+while count != break_point:
+    count -= 1
+    print(count)
+print(f'Fim: {count} igualou a {break_point}')
---
+++ testing_pass.py
@@ -0,0 +1,11 @@
+music_genres = ['Hip-hop', 'Rock', 'Rap', 'Pop']
+
+# for genre in music_genres:
+#     if genre == 'Rap':
+#         continue
+#     print({genre})
+
+for genre in music_genres:
+    if genre == 'Rock':
+        break
+    print(genre)
