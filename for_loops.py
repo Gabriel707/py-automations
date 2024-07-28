e7a8e02a38bb52fb8c6d18bb2f28e37b2b271f8a
Araujo <gabriel.araujo2@dellteam.com>
2024-07-27 21:23:30 -0300

    Nested loops exercise

---
+++ for_loops.py
@@ -0,0 +1,6 @@
+celulares = ['azus', 'samsung', 'Xiaomi', 'Iphone']
+versoes = ['Plus', 'Premium Plus', 'Premium Deluxe', 'Plus Premium Ultra']
+
+for celular in celulares:
+    for versao in versoes:
+        print(f'{celular.title()} - {versao}')
