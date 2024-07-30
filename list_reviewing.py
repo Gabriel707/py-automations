db59c87746c334a6e4ed2d5445e6ec98ed59554e
Gabriel Araujo <gabrielsant95@gmail.com>
2024-07-30 19:47:21 -0300

    Reviewing concepts.

--- list_reviewing.py
+++ list_reviewing.py
@@ -3,4 +3,14 @@ objetos_diarios = ['Celular', 'Garrafa', 'Chaves']
 for objeto in objetos_diarios:
     print(objeto)
 
-lista = [for i in range(10, 131)]
+lista = [i for i in range(10, 131)]
+
+print(lista, objetos_diarios)
+
+lista_de_listas = [
+    ['celular', 1500],
+    ['Garrafa', 30],
+    ['Chaves', 20]
+]
+
+print(lista_de_listas[1][0])
