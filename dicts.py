19904e7602c68750d1ee18f99f2bd3634aacfcf2
Gabriel707 <gabrielsant95@gmail.com>
2024-07-31 15:47:08 -0300

    Reviewing concepts

---
+++ dicts.py
@@ -0,0 +1,10 @@
+# Dictionary
+
+user = {'Name': 'John', 'Age': 18, 'Email': 'john@example.com'}
+
+# print(user.keys())
+# print(user.values())
+# print(user.items())
+
+for item in user.items():
+    print(item[0::])
