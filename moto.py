1c27ee4a195ab3572b56ccdecdeb62ad55f41ed2
Gabriel Araujo <gabrielsant95@gmail.com>
2024-08-03 18:26:57 -0300

    Manipulating JSON data

--- moto.py
+++ moto.py
@@ -2,6 +2,6 @@ def ligar_moto():
     print('Ligar moto')
 
 
-if __name__ == '__mai__':
+if __name__ == '__main__':
     print('Tirando moto da garagem')
     print(f'Estamos no arquivo {__name__}')
