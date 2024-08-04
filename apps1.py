aa154281c011b81ca3c1a943a2ce56dc80bd9820
Gabriel Araujo <gabrielsant95@gmail.com>
2024-08-04 16:50:58 -0300

    Manipulating JSON data

--- apps1.py
+++ apps1.py
@@ -21,4 +21,8 @@ with open('usuarios5.json', encoding="utf-8") as arquivo_json:
     data = json.load(arquivo_json)
     print(data[0]["admin"])
 
-    
+with open('desafio.json', encoding="utf-8") as arquivo_json:
+    data = json.load(arquivo_json)
+    print(data["user"][1]["email"])
+    print(data["user"][0]["address"]['city'])
+    print(data["user"][1]["orders"][0]["total"])
