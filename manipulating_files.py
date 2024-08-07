640966bdbcf13e5e3a3a661c321b29eca1d3e43a
Gabriel Araujo <gabrielsant95@gmail.com>
2024-08-07 11:50:12 -0300

    Converting collection

---
+++ manipulating_files.py
@@ -0,0 +1,10 @@
+import os
+
+# with open('celulares.txt', 'w') as arquivo:
+#     arquivo.write('Celular 1')
+
+nomes = ['Lucas', 'Carol', 'Julio', 'Gabriella']
+
+with open('nomes.txt', 'a', newline='') as arquivo:
+    for nome in nomes:
+        arquivo.write(nome + os.linesep)
