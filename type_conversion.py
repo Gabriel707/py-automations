138b9456fa35b5c0612f730b88077289d57e3622
Gabriel Araujo <gabrielsant95@gmail.com>
2024-08-07 11:36:30 -0300

    Converting collections

--- type_conversion.py
+++ type_conversion.py
@@ -1,2 +1,11 @@
-idade = input('Digite sua idade')
-print(int(idade > 17))
+idade = input('Digite sua idade ')
+print(int(idade) > (17))
+
+
+# conversions between collections
+
+greetings = "Hello"
+print(list(greetings))
+print(set(greetings))
+print(tuple(greetings))
+print(list(range(30)))
