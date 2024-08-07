a9e8fa6b760878f5a940422df60fdeb26222c4a8
Gabriel Araujo <gabrielsant95@gmail.com>
2024-08-07 11:51:58 -0300

    Creating and manipulating text files.

--- manipulating_files.py
+++ manipulating_files.py
@@ -3,7 +3,7 @@ import os
 # with open('celulares.txt', 'w') as arquivo:
 #     arquivo.write('Celular 1')
 
-nomes = ['Lucas', 'Carol', 'Julio', 'Gabriella']
+nomes = ['Lucas', 'Carol', 'Julio', 'Gabriella', 'Naluda']
 
 with open('nomes.txt', 'a', newline='') as arquivo:
     for nome in nomes:
